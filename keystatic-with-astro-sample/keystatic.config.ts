eb2e749039b139934650bc7515ca9d02b4f692af
Yoshikatsu Nishida <mail@nishida.dev>
2025-06-10 20:48:37 +0900

    feat: keystatic-with-astro-sampleを追加

---
+++ keystatic-with-astro-sample/keystatic.config.ts
@@ -0,0 +1,20 @@
+import { config, fields, collection } from '@keystatic/core';
+
+export default config({
+  storage: {
+    kind: 'github',
+    repo: 'nisshi-dev/headless-cms-playground'
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.markdoc({ label: 'Content' }),
+      },
+    }),
+  },
+});
